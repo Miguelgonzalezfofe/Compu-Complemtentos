f73deb0b51c965dd47c850458a774d43900c2ba1
Miguel Eduardo Gonzalez Fofe <247mmgg@gmail.com>
2024-02-09 12:34:07 -0300

    implementacion del sprend y carrito en localstorage junto con la interactividad de carrito de compras

--- javascript.js
+++ javascript.js
@@ -1,45 +1,9 @@
-/* 
-Registro usuario*
-login Usuario*
-array con objetos de los productos*
-rendirizacion de producto en html segun la API*
-carrito
-Boton de agregar al carrtio
-mostrar carrito de compra
-    nombre > precio 
-
-    total a pagar
-boton de comprar carrito
-    itilizar un procesador de pago con la api de mercado pago 
-filtro de productos
-
-
-footer contacto con formulario 
-*/
-/* variables */
-/* 
-cauchos
-carburador
-radiador
-bujia
-mortor
-bolante
-caja de cambios
-puerta 
-faros
- */
-
-
-
-
-
-
 
 /* VERIFICACION SI TENEMOS UN USUARIO EN LINEA */
-let usuario
 let usuarioEnLoSt = JSON.parse(localStorage.getItem("usuario"))
+
 /* DESESTRUCTURACION VARIABLES USUARIO */
-usuarioEnLoSt ? { nombre, apellido, correo, contraseña } = usuarioEnLoSt : console.log("no tenemos usuario")
+usuarioEnLoSt ? { nombre, apellido, correo, contraseña } = usuarioEnLoSt : abrirRegistro()
 
 /* EVENTO ABRIR REGISTRO DE USUARIO */
 let registroDeUsuario = document.getElementById("registroDeUsuario").addEventListener("click", () => abrirRegistro())
@@ -49,16 +13,17 @@ let ingresoDeUsuario = document.getElementById("inicioSesionUsuario").addEventLi
 /* EVENTO CERRAR REGISTRO DE USUARIO */
 let cierreRegistroUsuario = document.getElementById("cerrarRegistroUsuario").addEventListener("click", () => cerrarRegistro())
 
+/* EVENTO CERRAR INGRESO USUARIO */
 let cierreIngresoUsuario = document.getElementById("cerrarIngresoUsuario").addEventListener("click", () => cerrarIngreso())
 
-
+/* CONSTRUCTOR DE USURIO */
 class registroUsuario {
-    constructor(nombre, apellido, correo, contraseña, termYCond) {
+    constructor(nombre, apellido, correo, contraseña) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.correo = correo;
         this.contraseña = contraseña
-        this.termYcond = termYCond
+
     }
 }
 
@@ -79,7 +44,6 @@ let regNomUsu = document.getElementById("registroNombreUsuario").addEventListene
 let regApellUsu = document.getElementById("registroApellidoUsuario").addEventListener("change", (event) => { regApellUsu = event.target.value })
 let regCorreUsu = document.getElementById("registroCorreoUsuario").addEventListener("change", (event) => { regCorreUsu = event.target.value })
 let regcontrUsu = document.getElementById("registroContraseñaUsuario").addEventListener("change", (event) => { regcontrUsu = event.target.value })
-let terYConUsu = document.getElementById("terminosYCondiciones").addEventListener("change", (event) => { teryconUsu = event.target.value })
 
 
 /* BOTON DE CRACION DE USUARIO */
@@ -87,7 +51,7 @@ let btnRegistrase = document.getElementById("btnRegistrase").addEventListener("c
 
 /* FUNCION DE CRACION DEL USUARIO */
 function creacionUsuario() {
-    usuarioEnLoSt = new registroUsuario(regNomUsu, regApellUsu, regCorreUsu, regcontrUsu, terYConUsu)
+    usuarioEnLoSt = new registroUsuario({ regNomUsu, regApellUsu, regCorreUsu, regcontrUsu })
     localStorage.setItem("usuario", JSON.stringify(usuarioEnLoSt))
     // console.log(usuario)
     cerrarRegistro()
@@ -118,16 +82,14 @@ let btnIngreso = document.getElementById("btnIngreso").addEventListener("click",
 function ingresoUsuario() {
     validacionDatos() ? cambioDePerfil() : console.log("no ingresaste")
 
-/* falta agregar una confirmacion de correo  */
 }
-console.log(usuarioEnLoSt)
+
 function validacionDatos() {
     if (validacionContraseña() && validacionCorreo()) {
         return true
     } else {
         return false
     }
-    // validacionContraseña() 
 }
 function validacionCorreo() {
     if (ingCorrUsu == correo) {
@@ -150,15 +112,15 @@ function cambioDePerfil() {
     perfil.innerHTML = `<p class="perfil_iniciado">${nombre} ${apellido}</p>`
     cerrarIngreso()
 }
+
 /* FINAL LOCICA INGRESO USUARIO */
 
-
 /* PRODUCTOS */
-
+/* CREADOR DE PRODUCTOS */
 class nuevoProducto {
     constructor(id, nombre, precio, img) {
         this.id = id,
-            this.nombre = nombre;
+        this.nombre = nombre;
         this.precio = precio;
         this.img = img
     }
@@ -238,7 +200,17 @@ const productos = [
         "img": "https://images.start.com.ar/CRU-CT240BX500SSD1.jpg"
     }
 ]
+let cerrarCarr = document.getElementById("cerrarCarrito").addEventListener("click",()=> cerrarCarrito())
+let abrirCarr = document.getElementById("carritoCompras").addEventListener("click",()=> abrirCarrito())
 
+function cerrarCarrito(){
+    let ventanaCarrito = document.getElementById("contenedorCarrito")
+    ventanaCarrito.style.display = "none"
+}
+function abrirCarrito(){
+    ventanaCarrito = document.getElementById("contenedorCarrito")
+    ventanaCarrito.style.display = "flex"
+}
 
 /* CREACION DE LOS PRODUCTOS EN LA WEB */
 let padreCajaProductos = document.getElementById("cajaProductos")
@@ -257,35 +229,33 @@ for (const x of productos) {
     padreCajaProductos.append(div)
 }
 /* SECCION PARA MOSTRAR EL CARRITO DE COMPRAS */
+
 /* variable carrito donde se guardan */
 let carrito = []
+
 /* CARRITO EN EL LOCAL STORAGE */
 let carritoEnLoSt = JSON.parse(localStorage.getItem("carrito"))
 
-/* EVENTO DE BOTONES AGREGAR PRODUCTOS */
-let btn1 = document.getElementById("btn1").addEventListener("click", () => agregarAlCarrito(productos[0]))
-let btn2 = document.getElementById("btn2").addEventListener("click", () => agregarAlCarrito(productos[1]))
-let btn3 = document.getElementById("btn3").addEventListener("click", () => agregarAlCarrito(productos[2]))
-let btn4 = document.getElementById("btn4").addEventListener("click", () => agregarAlCarrito(productos[3]))
-let btn5 = document.getElementById("btn5").addEventListener("click", () => agregarAlCarrito(productos[4]))
-let btn6 = document.getElementById("btn6").addEventListener("click", () => agregarAlCarrito(productos[5]))
-let btn7 = document.getElementById("btn7").addEventListener("click", () => agregarAlCarrito(productos[6]))
-let btn8 = document.getElementById("btn8").addEventListener("click", () => agregarAlCarrito(productos[7]))
-let btn9 = document.getElementById("btn9").addEventListener("click", () => agregarAlCarrito(productos[8]))
-let btn10 = document.getElementById("btn10").addEventListener("click", () => agregarAlCarrito(productos[9]))
-let btn11 = document.getElementById("btn11").addEventListener("click", () => agregarAlCarrito(productos[10]))
-let btn12 = document.getElementById("btn12").addEventListener("click", () => agregarAlCarrito(productos[11]))
 
+/* EVENTO DE BOTONES AGREGAR PRODUCTOS */
+for (let i = 0; i < productos.length; i++) {
+    let btn = document.getElementById(`btn${productos[i].id}`);
+    btn.addEventListener("click", () => agregarAlCarrito({ ...productos[i] }));
+}
 
 
 /* FUNCION AGREGAR EL PRODUCTO AL CARRITO */
 function agregarAlCarrito(valor) {
     carrito.push(valor)
+    carritoEnLoSt = carrito
+    localStorage.setItem("carrito",JSON.stringify(carritoEnLoSt))
     mostarCarrtio()
 }
+
 /* FUNCION PARA MOSTRAR EL CARRITO DE COMPRAS EN PANTALLA */
 function mostarCarrtio() {
     carritoEnHtml()
+
 }
 
 let PantallaCarrito = document.getElementById("carrito")
@@ -299,19 +269,27 @@ function carritoEnHtml() {
         PantallaCarrito.append(ul)
     }
     sumarCarrito()
+    productoMasCaro()
 }
-let preciodelcarrito = []
-function sumarCarrito (){
+/* AGREGAR SOLO EL PRECIO DE LOS PRODUCTOS A UNA VARIABLE */
+let preciodelcarrito
+function sumarCarrito() {
     preciodelcarrito = []
-    for(x of carrito){
-    preciodelcarrito.push(x.precio)
+    for (x of carrito) {
+        preciodelcarrito.push(x.precio)
+    }
+    /* SUMAR TODOS LOS PRECIOS DE LOS PRODUCTOS */
+    let total = preciodelcarrito.reduce((acc, num) => acc + num)
+    let precioFinal = document.getElementById("totalCarrito")
+    /* CAMBIO DEL TEXTO CON EL DOM */
+    precioFinal.innerText = total
 }
-let total = preciodelcarrito.reduce((acc,num)=> acc+num)
-let precioFinal = document.getElementById("totalCarrito")
-precioFinal.innerText= total
-console.log(preciodelcarrito)
-console.log(total)
+
+/* PRODUCTO MAS CARO CON EM METODO SPREND */
+function productoMasCaro() {
+    /* BUSCAR EL MAYOR */
+    let masCaro = Math.max(...preciodelcarrito)
+    /* CAMBIO DEL TEXTO CON EL DOM */
+    let mayorValor = document.getElementById("mayorValor")
+    mayorValor.innerText = masCaro
 }
-
-
-
